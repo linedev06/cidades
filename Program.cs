ba3e76479ff5a33faa9e7b1483aa4e4af65b728c
linedev06 <vitorafelismino@gmail.com>
2023-03-10 19:57:26 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("cidades do litoral");
+Console.WriteLine("1- Praia Grande");
+Console.WriteLine("2- Mongaguá");
+Console.WriteLine("3- Itanhaém");
+Console.WriteLine("4- Peruíbe");
+
+
+
+
+
+
+
